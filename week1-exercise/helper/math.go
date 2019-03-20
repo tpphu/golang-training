4259698e47746c24657f089fc17eea820b042f52
Tran Phong Phu <vietean@gmail.com>
2019-03-20 19:51:22 +0700

    * Refactor Tien's code

--- week1-exercise/helper/math.go
+++ week1-exercise/helper/math.go
@@ -33,10 +33,52 @@ func Max(list interface{}) interface{} {
 
 type convert1 func(int) int
 
-func Map2(list []int, fn convert1) []int {
-	result := make([]int, len(list))
-	for i := 0; i < len(list); i++ {
-		result[i] = fn(list[i])
+// func Map2(list []int, fn convert1) []int {
+// 	result := make([]int, len(list))
+// 	for i := 0; i < len(list); i++ {
+// 		result[i] = fn(list[i])
+// 	}
+// 	return result
+// }
+// list := []int{1, 2, 3}
+func Map2(list interface{}, fn interface{}) interface{} {
+	// 1. Slice or Array
+	// 1.1 Lay value of interface
+	listValue := reflect.ValueOf(list)
+	// 1.2 Lay cai
+	listType := listValue.Type()
+	listKind := listType.Kind()
+
+	if listKind != reflect.Slice && listKind != reflect.Array {
+		panic("list should be slice or array")
 	}
-	return result
+
+	// 2
+	fnValue := reflect.ValueOf(fn)
+	fnType := fnValue.Type()
+	outType := fnType.Out(0)
+
+	// 3. Tao ra ra retResult
+	// 3.1 Tao ra cai sliceType
+	outSliceType := reflect.SliceOf(outType)
+	retResult := reflect.MakeSlice(outSliceType, 0, 0)
+
+	// 4. Loop qua list
+	for i := 0; i < listValue.Len(); i++ {
+		item := listValue.Index(i)
+		// 4.1 Tao input cho func
+		in := []reflect.Value{item}
+		// 4.2 Call function
+		result := fnValue.Call(in)[0]
+		// 4.3 Append vao ket qua tu buoc 3 tao ra
+		retResult = reflect.Append(retResult, result)
+	}
+	// 5. Tra ve du lieu
+	return retResult.Interface()
 }
+
+// list := []interface{}{1, false, "phu"}
+// func Map3(list []interface{}, fn interface{}) interface{} {
+
+// 	return nil
+// }
--- week1-exercise/helper/math_test.go
+++ week1-exercise/helper/math_test.go
@@ -1,6 +1,10 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestMax_1(t *testing.T) {
 	v := []int{1, 2, 3}
@@ -35,6 +39,8 @@ func TestMax_3(t *testing.T) {
 	}
 }
 
+// 1. Ham nay dang dung
+// 2. Minh can phai refactor cho no tong qua
 func TestMap2_1(t *testing.T) {
 	v := make([]int, 5)
 	v[0] = 1
@@ -43,11 +49,42 @@ func TestMap2_1(t *testing.T) {
 	v[3] = 7
 	v[4] = 8
 
-	result1 := Map2(v, func(x int) int { return x * 3 })
+	expected := []int{3, 15, 9, 21, 24}
+
+	actual := Map2(v, func(x int) int { return x * 3 })
+	fmt.Println("actual", actual)
 	//_ = result1 // declared and not used
-	for i := 0; i < len(v); i++ {
-		if result1[i] != v[i]*3 {
-			t.Error("khong bang")
-		}
+	// for i := 0; i < len(v); i++ {
+	// 	if result1[i] != v[i]*3 {
+	// 		t.Error("khong bang")
+	// 	}
+	// }
+
+	if reflect.DeepEqual(actual, expected) != true {
+		t.Error("Actual should be expected")
+	}
+}
+
+func TestMap2_2(t *testing.T) {
+	v := []int{1, 2, 3}
+
+	expected := []int{2, 4, 6}
+
+	actual := Map2(v, func(x int) int { return x * 2 })
+
+	if reflect.DeepEqual(actual, expected) != true {
+		t.Error("Actual should be expected")
+	}
+}
+
+func TestMap2_3_Count_String_Len(t *testing.T) {
+	v := []string{"a", "bb", "ccc"}
+
+	expected := []int{1, 2, 3}
+
+	actual := Map2(v, func(elem string) int { return len(elem) })
+
+	if reflect.DeepEqual(actual, expected) != true {
+		t.Error("Actual should be expected")
 	}
 }
