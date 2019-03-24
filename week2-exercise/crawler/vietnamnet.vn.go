e229f7cb031170c7fa3ca18aebf23f7de2b41364
Tran Phong Phu <vietean@gmail.com>
2019-03-24 11:01:58 +0700

    * Remove log

--- week2-exercise/crawler/vietnamnet.vn.go
+++ week2-exercise/crawler/vietnamnet.vn.go
@@ -1,7 +1,6 @@
 package crawler
 
 import (
-	"fmt"
 	"regexp"
 	"strings"
 	"time"
@@ -17,7 +16,6 @@ var extractPublishDateVietNamNet = func(selector string, doc *goquery.Document) 
 	publishedDateStr := strings.TrimSpace(extract(selector, doc))
 	r, _ := regexp.Compile("[^0-9/:+GMT]+")
 	publishedDateStr = string(r.ReplaceAll([]byte(publishedDateStr), []byte(" ")))
-	fmt.Println("publishedDateStr:", publishedDateStr)
 	// Value: 24/03/2019 07:50 GMT+7
 	// Format: Mon Jan 2 15:04:05 MST 2006 | 2006-01-02T15:04:05.000Z
 	publishedDate, _ := time.Parse("02/01/2006 15:04 MST", publishedDateStr)
