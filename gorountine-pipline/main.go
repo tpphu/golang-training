82423fdb596086ae639415e1f814a9cce8007e04
Tran Phong Phu <vietean@gmail.com>
2019-03-24 20:52:09 +0700

    + Da thu nghiem va kiem chung la dung, chinh do cach Println sai thoi

--- gorountine-pipline/main.go
+++ gorountine-pipline/main.go
@@ -5,17 +5,19 @@ import (
 	"time"
 )
 
+// https://gobyexample.com/channel-buffering
 // Nhan vao mot danh sach cac so nguyen
 // Tra ra mot channel
 func gen(nums ...int) <-chan int {
 	// Run Step 1
-	out := make(chan int, 1) //capacity
+	out := make(chan int, 10) //capacity
 	// Run Step 2
 	go func() {
 		for _, n := range nums {
 			// Chi day dc vao channel khi ma len < capacity
-			fmt.Print("\nKhi nao in ra?\n")
 			out <- n
+			// In ra o day co nghia la da push dc
+			fmt.Printf("\n[GEN] channel: %d", n)
 		}
 		close(out)
 	}()
@@ -26,14 +28,11 @@ func gen(nums ...int) <-chan int {
 func sq(in <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
-		//fmt.Println("in:", in)
-		// for n := range in {
-		// 	time.Sleep(time.Second * 1)
-		// 	out <- n * n
-		// }
-		n := <-in
-		out <- n * n
-		//close(out)
+		for n := range in {
+			out <- n * n
+			fmt.Printf("\n[SQ] channel: %d", n*n)
+		}
+		close(out)
 	}()
 	return out
 }
@@ -46,13 +45,10 @@ func main() {
 	// Consume the output.
 	go func() {
 		for v := range out {
-			//time.Sleep(1 * time.Second)
-			fmt.Println(v)
+			time.Sleep(1 * time.Second)
+			fmt.Printf("\n[OUT]: %d", v)
 		}
 	}()
-	// fmt.Println(<-out) // 4
-	// fmt.Println(<-out) // 9
-	// fmt.Println(<-out) // 16
 	time.Sleep(30 * time.Second)
 
 }
