dff37775323cff8bddecff0d4db3d6e16986ed0b
Tran Phong Phu <vietean@gmail.com>
2019-03-27 00:15:37 +0700

    * Move logic watcher to helper

--- week2-exercise/main.go
+++ week2-exercise/main.go
@@ -8,7 +8,7 @@ import (
 	"./crawler"
 	"./helper"
 	"./model"
-	tm "github.com/buger/goterm"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -16,35 +16,6 @@ import (
 var SaiGonTime crawler.ICrawler = crawler.CreateSaiGonTimeCrawler()
 var VietNamNet crawler.ICrawler = crawler.CreateVietNamNetCrawler()
 var DBConnectString = "default:secret@/crawler?charset=utf8&parseTime=True&loc=Local"
-
-type Watcher struct {
-	//load info
-	DBLoadUrlReq   int
-	DBLoadUrlRes   int
-	DBLoadUrlErr   int
-	DBLoadUrlTotal int
-	//download info
-	NumHTTPReq int
-	NumHTTPRes int
-	NumHTTPErr int
-	//update info
-	DBInsArticleReq int
-	DBInsArticleRes int
-	DBInsArticleErr int
-	//save info
-}
-
-func (self *Watcher) GetDBLoadUrlInfo() string {
-	return fmt.Sprintf("%d/%d/%d", self.DBLoadUrlReq, self.DBLoadUrlRes, self.DBLoadUrlErr)
-}
-
-func (self *Watcher) GetHTTPDownloadInfo() string {
-	return fmt.Sprintf("%d/%d/%d", self.NumHTTPReq, self.NumHTTPRes, self.NumHTTPErr)
-}
-
-func (self *Watcher) GetDBInsArticleInfo() string {
-	return fmt.Sprintf("%d/%d/%d", self.DBInsArticleReq, self.DBInsArticleRes, self.DBInsArticleErr)
-}
 
 func main() {
 	db, err := gorm.Open("mysql", DBConnectString)
@@ -54,31 +25,19 @@ func main() {
 	db.LogMode(false)
 	db.AutoMigrate(&model.Url{}, &model.Article{})
 
-	watcher := &Watcher{}
+	watcher := &helper.Watcher{}
 	urlCrawlChan := load(db, watcher)
 	articleChan, urlUpdateChan := crawl(db, watcher, urlCrawlChan)
 	save(db, watcher, articleChan)
 	update(db, watcher, urlUpdateChan)
 
 	for {
-		tm.Clear()
-		tm.MoveCursor(1, 1)
-		// Based on http://golang.org/pkg/text/tabwriter
-		totals := tm.NewTable(0, 10, 5, ' ', 0)
-		fmt.Fprintf(totals, "[DB]LoadUrl\t[DB]LoadUrlTotal\t[HTTP]Download\t[DB]InsArticle\n")
-		fmt.Fprintf(totals, "%s\t%d\t%s\t%s\n",
-			watcher.GetDBLoadUrlInfo(),
-			watcher.DBLoadUrlTotal,
-			watcher.GetHTTPDownloadInfo(),
-			watcher.GetDBInsArticleInfo())
-		tm.Println(totals)
-
-		tm.Flush()
+		watcher.Out()
 		time.Sleep(1 * time.Second)
 	}
 }
 
-func load(db *gorm.DB, watcher *Watcher) <-chan model.Url {
+func load(db *gorm.DB, watcher *helper.Watcher) <-chan model.Url {
 	urlCrawlChan := make(chan model.Url, 10)
 	go func() {
 		for {
@@ -106,7 +65,7 @@ func load(db *gorm.DB, watcher *Watcher) <-chan model.Url {
 	return urlCrawlChan
 }
 
-func crawl(db *gorm.DB, watcher *Watcher, urlCrawlChan <-chan model.Url) (<-chan model.Article, <-chan model.Url) {
+func crawl(db *gorm.DB, watcher *helper.Watcher, urlCrawlChan <-chan model.Url) (<-chan model.Article, <-chan model.Url) {
 	articleChan := make(chan model.Article, 10)
 	urlUpdateChan := make(chan model.Url, 10)
 	go func() {
@@ -135,7 +94,7 @@ func crawl(db *gorm.DB, watcher *Watcher, urlCrawlChan <-chan model.Url) (<-chan
 	return articleChan, urlUpdateChan
 }
 
-func save(db *gorm.DB, watcher *Watcher, articleChan <-chan model.Article) {
+func save(db *gorm.DB, watcher *helper.Watcher, articleChan <-chan model.Article) {
 	go func() {
 		for article := range articleChan {
 			watcher.DBInsArticleReq++
@@ -148,7 +107,7 @@ func save(db *gorm.DB, watcher *Watcher, articleChan <-chan model.Article) {
 	}()
 }
 
-func update(db *gorm.DB, watcher *Watcher, urlUpdateChan <-chan model.Url) {
+func update(db *gorm.DB, watcher *helper.Watcher, urlUpdateChan <-chan model.Url) {
 	go func() {
 		for url := range urlUpdateChan {
 			url.State = model.UrlStateRunning
