091a99c79a2a36621c3f162ec6317fba2ecf2564
Tran Phong Phu <vietean@gmail.com>
2019-03-30 20:26:00 +0700

    * Update testing

--- week3-exercise/k6/script.js
+++ week3-exercise/k6/script.js
@@ -8,3 +8,11 @@ export default function() {
     "transaction time OK": (r) => r.timings.duration < 10
   });
 };
+
+// export default function() {
+//   let res = http.get("http://host.docker.internal:8081/note/1");
+//   check(res, {
+//     "status was 200": (r) => r.status == 200,
+//     "transaction time OK": (r) => r.timings.duration < 10
+//   });
+// };
