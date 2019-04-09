9a711b21cc8f7463532f8d7ec836bf65ec0ef763
Tran Phong Phu <vietean@gmail.com>
2019-04-09 16:57:27 +0700

    * Nginx does not work

--- week3-exercise/k6/get-increment-id.js
+++ week3-exercise/k6/get-increment-id.js
@@ -3,8 +3,9 @@ import { check } from "k6";
 
 var allUniqueIds = {};
 export default function() {  
-  let res = http.get("http://host.docker.internal:8081/get-increment-id");
+  // let res = http.get("http://host.docker.internal:8081/get-increment-id");
   // let res = http.get("http://api.dev.local/get-increment-id");
+  let res = http.get("http://127.0.0.1:8082/get-increment-id");
   check(res, {
     "status was 200": (r) => r.status == 200,
     "transaction time OK": (r) => r.timings.duration < 50,
