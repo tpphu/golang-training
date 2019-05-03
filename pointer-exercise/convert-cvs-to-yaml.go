6213d1eeac257e5ecd765ca45c1e2bf6eacffc72
Tran Phong Phu <phutp@sendo.vn>
2019-05-03 14:26:42 +0700

    + Demo pointer

---
+++ pointer-exercise/convert-cvs-to-yaml.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type Ward struct {
+	Id      int    `yaml:"id"`
+	Name    string `yaml:"name"`
+	Deleted bool   `yaml:"deleted,omitempty"`
+}
+
+type District struct {
+	Id      int     `yaml:"id"`
+	Name    string  `yaml:"name"`
+	Deleted bool    `yaml:"deleted,omitempty"`
+	Wards   []*Ward `yaml:"wards"`
+}
+
+type Region struct {
+	Id        int         `yaml:"id"`
+	Name      string      `yaml:"name"`
+	Deleted   bool        `yaml:"deleted,omitempty"`
+	Districts []*District `yaml:"districts"`
+}
+
+var mapRegion map[int]*Region = map[int]*Region{}
+var mapDistrict map[int]*District = map[int]*District{}
+
+func convertCSVToYAML(fn string) (data []*Region, err error) {
+	file, err := os.Open(fn)
+	defer file.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	reader := bufio.NewReader(file)
+	for {
+		var buffer bytes.Buffer
+
+		var l []byte
+		var isPrefix bool
+		for {
+			l, isPrefix, err = reader.ReadLine()
+			buffer.Write(l)
+			if !isPrefix {
+				break
+			}
+			if err != nil {
+				break
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		line := buffer.String()
+		parts := strings.Split(line, ",")
+		if len(parts) != 6 {
+			panic("Line is not correct | data: " + line)
+		}
+		if parts[0] == "" || parts[0] == "Mã Tỉnh" {
+			continue
+		}
+		regionId, _ := strconv.Atoi(parts[0])
+		regionName := parts[1]
+		districtId, _ := strconv.Atoi(parts[2])
+		districtName := parts[3]
+		wardId, _ := strconv.Atoi(parts[4])
+		wardName := parts[5]
+		region, ok := mapRegion[regionId]
+		if !ok {
+			region = &Region{
+				Id:   regionId,
+				Name: regionName,
+			}
+			mapRegion[regionId] = region
+			data = append(data, region)
+		}
+		district, ok := mapDistrict[districtId]
+		if !ok {
+			district = &District{
+				Id:   districtId,
+				Name: districtName,
+			}
+			mapDistrict[districtId] = district
+			region.Districts = append(region.Districts, district)
+		}
+
+		ward := &Ward{
+			Id:   wardId,
+			Name: wardName,
+		}
+		district.Wards = append(district.Wards, ward)
+	}
+
+	if err != io.EOF {
+		fmt.Printf(" > Failed!: %v\n", err)
+	}
+
+	return data, nil
+}
+
+func main() {
+	file := "./data.csv"
+	regions, _ := convertCSVToYAML(file)
+	data, err := yaml.Marshal(&regions)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(data))
+}
