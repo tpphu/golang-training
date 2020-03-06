c592e77bdedaefa1d6e5dc236cba246869e4c397
Phu Tran Phong <phutp@sendo.vn>
2020-03-06 23:05:11 +0700

    * Update

---
+++ week2-exercise/app/app.go
@@ -0,0 +1,10 @@
+package app
+
+import "github.com/jinzhu/gorm"
+
+type App struct {
+	db *gorm.DB
+}
+
+type App struct {
+}
--- week2-exercise/main.go
+++ week2-exercise/main.go
@@ -26,6 +26,17 @@ func main() {
 			fmt.Println("Welcome to Crawler Service. To see how to use, type -h for more detail")
 			return nil
 		},
+		Before: func(c *cli.Context) error {
+			db, err := gorm.Open("mysql", c.String("database"))
+			if err != nil {
+				panic(err)
+			}
+			// db.DB().SetMaxIdleConns(2)
+			// db.DB().SetMaxOpenConns(10)
+			db.LogMode(c.Bool("dblog"))
+			c.App.Metadata["db"] = db
+			return nil
+		},
 		Flags: []cli.Flag{
 			&cli.IntFlag{
 				Name:    "port",
@@ -54,6 +65,7 @@ func main() {
 				Usage:   "Start the service",
 				Action: func(c *cli.Context) error {
 					fmt.Println("Start the service")
+					// db := c.App.Metadata["db"].(*gorm.DB)
 					return nil
 				},
 			},
@@ -62,11 +74,7 @@ func main() {
 				Aliases: []string{"m"},
 				Usage:   "Migrate schema to DB",
 				Action: func(c *cli.Context) error {
-					db, err := gorm.Open("mysql", c.String("database"))
-					if err != nil {
-						panic(err)
-					}
-					db.LogMode(c.Bool("dblog"))
+					db := c.App.Metadata["db"].(*gorm.DB)
 					db.DropTableIfExists(&model.Url{}, &model.Article{})
 					db.AutoMigrate(&model.Url{}, &model.Article{})
 					return nil
@@ -77,18 +85,29 @@ func main() {
 				Aliases: []string{"s"},
 				Usage:   "Seed data to DB",
 				Action: func(c *cli.Context) error {
-					db, err := gorm.Open("mysql", c.String("database"))
-					if err != nil {
-						panic(err)
+					db := c.App.Metadata["db"].(*gorm.DB)
+					urls := []string{
+						"https://vnexpress.net/the-gioi/iran-ghi-nhan-ky-luc-hon-1-200-ca-nhiem-ncov-moi-4065455.html",
+						"http://tiasang.com.vn/-quan-ly-khoa-hoc/Thay-doi-trong-dau-tu-congtu-cho-khoa-hoc-20767",
+						"https://www.thesaigontimes.vn/td/300790/doanh-nghiep-lam-nguy-vi-e-am-chu-cho-thue-cung-lao-dao.html",
+						"https://www.thesaigontimes.vn/300783/chong-dich-nuoc-bot-khong-the-bang-nuoc-bot.html",
+						"https://www.thesaigontimes.vn/td/300773/doanh-nghiep-noi-dia-dung-truoc-noi-lo-thau-tom.html",
+						"https://www.thesaigontimes.vn/300849/mua-sam-qua-mang-xa-hoi-o-tphcm-chiem-50-mua-hang-truc-tuyen.html",
+						"https://www.thesaigontimes.vn/300839/goi-tin-dung-285000-ti-dong-ho-tro-doanh-nghiep-trong-dich-covid-19.html",
+						"https://www.thesaigontimes.vn/td/300743/nguoc-chieu-thi-truong-tang-truong-loi-nhuan-bat-dong-san-vuot-ca-ngan-hang.html",
+						"https://www.thesaigontimes.vn/td/300828/khach-san-nho-o-tphcm-cam-cu-voi-khach-le-khach-theo-gio.html",
+						"https://www.thesaigontimes.vn/300808/novaland-gui-don-%E2%80%98keu-cuu%E2%80%99-bo-xay-dung-tra-ve-tphcm.html",
+						"https://www.thesaigontimes.vn/300840/thu-tuong-yeu-cau-giai-phap-de-khan-truong-phuc-hoi-du-lich-hang-khong-.html",
+						"https://www.thesaigontimes.vn/300828/khach-san-nho-o-tphcm-cam-cu-voi-khach-le-khach-theo-gio.html",
+						"https://www.thesaigontimes.vn/300821/soc-trang-co-nha-may-dien-gio-thu-2-hon-5300-ti-dong.html",
+						"https://www.thesaigontimes.vn/300719/resort-moi-o-nam-hoi-an-khai-truong-trong-mua-vang-khach-vi-covid-19.html",
+						"https://www.thesaigontimes.vn/300775/ung-pho-voi-dich-benh-dung-lam-theo-cam-xuc.html",
+						"https://www.thesaigontimes.vn/300856/chiu-nhieu-suc-ep-facebook-can-nhac-dieu-chinh-du-an-tien-ao-libra.html",
 					}
-					db.LogMode(c.Bool("dblog"))
-					urls := []model.Url{model.Url{
-						URL: "https://vnexpress.net/the-gioi/iran-ghi-nhan-ky-luc-hon-1-200-ca-nhiem-ncov-moi-4065455.html",
-					}, model.Url{
-						URL: "http://tiasang.com.vn/-quan-ly-khoa-hoc/Thay-doi-trong-dau-tu-congtu-cho-khoa-hoc-20767",
-					}}
 					for i := 0; i < len(urls); i++ {
-						db.Create(&urls[i])
+						db.Create(&model.Url{
+							URL: urls[i],
+						})
 					}
 					return nil
 				},
