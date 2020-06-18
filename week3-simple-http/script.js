bf1b812ca639a8645fa5a60d7e9e19cb5688c758
Phu Tran Phong <phutp@sendo.vn>
2020-06-18 16:05:02 +0700

    + Add GRPC

--- week3-simple-http/script.js
+++ week3-simple-http/script.js
@@ -1,13 +1,13 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
-
+// k6.io
 export let options = {
-  vus: 20,
-  iterations: 300000,
+  vus: 20, // 20 concurrency
+  iterations: 300000, // 300k request
 };
 
 export default function() {
-  let res =  http.get('http://localhost:3001/safe-counter');
+  let res =  http.get('http://localhost:3000/safe-counter');
   check(res, { 'status was 200': r => r.status == 200 });
 }
