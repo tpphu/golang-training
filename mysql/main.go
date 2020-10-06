10e5608658059deaf037742020897952bc811d90
Phu Tran Phong <phu.tp@teko.vn>
2020-10-06 22:53:00 +0700

    Test atomic update mysql

--- mysql/main.go
+++ mysql/main.go
@@ -3,20 +3,23 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, _ := sql.Open("mysql", "default:secret@/code_service")
-	result, _ := db.Exec("insert into code(`min`,`max`) " +
-		" select 21, 30 " +
-		" from code " +
-		" where 0 = (select count(id) " +
-		" from code " +
-		" where (21 <= `max`) AND 30 >= `min`) " +
-		" LIMIT 1")
-
-	rowAffect, _ := result.RowsAffected()
-	fmt.Println(rowAffect)
+	fmt.Println("1")
+	db, err := sql.Open("mysql", "default:secret@(127.0.0.1:3306)/dogfood")
+	fmt.Println("2")
+	fmt.Println(db, err)
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	err=db.Ping()
+	fmt.Println("test", err)
 }
