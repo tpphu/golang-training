081917e7086f8e8e3d0cdf181ad2e34a480504ef
Phu Tran Phong <phu.tp@teko.vn>
2020-10-13 20:38:48 +0700

    + Better refactor

--- week2/goroutine/ex1.go
+++ week2/goroutine/ex1.go
@@ -1,45 +1,35 @@
 package goroutine
 
 import (
-	"fmt"
 	"sync"
 )
 
-func Sum(arr []int) int { // CPU1
+func Sum(arr []int) int {
 	part1 := arr[:len(arr)/2]
 	part2 := arr[len(arr)/2:]
 	var sum1 int
 	var sum2 int
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
-	go sum(part1, &sum1, wg, "sum1") // CPU2
-	go sum(part2, &sum2, wg, "sum2") // CPU3
-	// CPU1 wait
+	wg := sync.WaitGroup{}
+	// Group 1
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		sum1 = sum(part1)
+	}()
+	// Group 2
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		sum2 = sum(part2)
+	}()
 	wg.Wait()
-	// if sum1 != nil && sum2 != nil {
-	// 	return *sum1 + *sum2
-	// }
-	// return 0
 	return sum1 + sum2
 }
 
-func sum(arr []int, result *int, wg *sync.WaitGroup, key string) int {
-	defer wg.Done()
-	defer func() {
-		fmt.Printf("result | %s | 1\n", key)
-	}()
-	defer func() {
-		fmt.Printf("result | %s | 2\n", key)
-	}()
-	fmt.Println("key:", key)
+func sum(arr []int) int {
 	ret := 0
 	for _, v := range arr {
 		ret += v
 	}
-	if ret > 10 {
-		return 0
-	}
-	*result = ret
-	fmt.Printf("result | %s: %d\n", key, *result)
 	return ret
 }
