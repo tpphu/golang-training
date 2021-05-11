7f4454aa633d4c1ff8a3bc5ffcd3023e04f2bd7f
Phu Tran Phong <phu.tp@teko.vn>
2021-05-11 21:48:05 +0700

    * Update

--- week2/helper/math.go
+++ week2/helper/math.go
@@ -1,8 +1,40 @@
 package helper
 
+type Persion interface {
+	GetName() string
+	GetAge() int
+}
+
 func Max(n ...interface{}) interface{} {
+	for i := 0; i < len(n); i++ {
+		v := n[i]
+	}
 	return nil
 }
+
+type Fn func(int) int
+
+func demo() {
+	ch := make(chan Person, 10)
+	ch <- 1
+	n := <-ch // Co thang day vao, hoac la channel bi closed
+}
+
+func demo2() {
+	ch := make(chan Fn, 10)
+	ch <- 1
+	n := <-ch
+}
+
+// func demo() {
+// 	ch := make(chan URL, 10)
+// 	ch := make(chan Data, 10)
+// 	ch <- 1
+// 	n := <-ch
+// 	if n == 1 {
+
+// 	}
+// }
 
 // func Max2(n1 interface{}, n2 interface{}) interface{} {
 // 	return nil
